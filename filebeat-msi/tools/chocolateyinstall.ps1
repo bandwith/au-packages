cc11f5357cd19c7c5979ace43343c72f52865c08
Sam Kuonen <sam.kuonen@oit.gatech.edu>
2020-09-24 19:44:12 -0400

    grab checksum from elastic.co

---
+++ filebeat-msi/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+
+$ErrorActionPreference = 'Stop';
+
+$packageName  = 'filebeat-msi'
+$toolsDir     = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$url          = 'https://somewhere.com/file.msi'
+$url64        = 'https://somewhere.com/file-x64.msi'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'msi'
+  url            = $url
+  url64bit       = $url64
+  silentArgs     = "/qn /norestart"
+  validExitCodes = @(0, 3010, 1641)
+  softwareName   = 'Beats filebeat*'
+  checksum       = '12345'
+  checksumType   = 'sha256'
+  checksum64     = '123356'
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
