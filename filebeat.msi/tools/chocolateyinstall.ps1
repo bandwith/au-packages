1939701a5980a91d76628ae687ba7696d7b63ae6
Sam Kuonen <sam.kuonen@oit.gatech.edu>
2020-09-26 23:29:10 -0400

    update appveyor settings, edit nuspec to exclude update.ps

---
+++ filebeat.msi/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageName  = 'filebeat-msi'
+$toolsDir     = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$url          = 'https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.9.2-windows-x86.msi'
+$url64        = 'https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.9.2-windows-x86_64.msi'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'msi'
+  url            = $url
+  url64bit       = $url64
+  silentArgs     = "/qn /norestart"
+  validExitCodes = @(0, 3010, 1641)
+  softwareName   = 'Beats filebeat*'
+  checksum       = '43519742faa8a61eeb2d6aafb9c979709bad4b5335be3a82c8cba351bc1aced78d0c9ce660e37bbd83370fbe4ee9ae6005288ad0992d083251754bf0d606fdb0'
+  checksumType   = 'sha512'
+  checksum64     = '36729cf2ff00f2cd32f5d8519db766a82c820a1f82ef63fd20016016e5bebfc2781d949f89a848a4d46ffb69e4a3b220fb3678c561e87aedf4c7da2dbdf11010'
+  checksumType64 = 'sha512'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ packetbeat.msi/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageName  = 'packetbeat-msi'
+$toolsDir     = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$url          = 'https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-7.9.2-windows-x86.msi'
+$url64        = 'https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-7.9.2-windows-x86_64.msi'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'msi'
+  url            = $url
+  url64bit       = $url64
+  silentArgs     = "/qn /norestart"
+  validExitCodes = @(0, 3010, 1641)
+  softwareName   = 'Beats packetbeat*'
+  checksum       = '97fa465c2b1c4f5c3c1ddd17ff45206f5f90290edaf6a43140f8899c922019fcd4f9b0fa6507fe3fbbe64e7864b3005418d70c24214120f5b07f01c92902034d'
+  checksumType   = 'sha512'
+  checksum64     = 'd92a24f59beb0bfc455dd27fe77ac25d1362f27f418da872bf152eab2804b3707ebb4696eef61037693d470e39a85639625343d114c0f051dd9c1d6c68ad5465'
+  checksumType64 = 'sha512'
+}
+
+Install-ChocolateyPackage @packageArgs
